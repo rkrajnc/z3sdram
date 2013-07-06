f78d085bfb3752b0237b33cb0b01a9a740d754ad
Administrator <Administrator@localhost>
2013-07-06 19:11:41 +0400

    fixed IOR/IOW timings with respect to TIOR3

--- isa.v
+++ isa.v
@@ -5,8 +5,10 @@ module isa (
 	read,
 	nSLAVEN,
 	
+	terminate,
+	
 	nIOR,
-	nIOW
+	nIOW,
 );
 
 input clk;
@@ -16,21 +18,34 @@ input en;
 input read;
 input nSLAVEN;
 
+input terminate;
 
 output nIOR;
 output nIOW;
 
+
+
 reg nior_r;
 reg niow_r;
+
+reg [3:0] en_delay;
 
 assign nIOR = nior_r | nSLAVEN;
 assign nIOW = niow_r | nSLAVEN;
 
 always @(posedge clk) begin
-	if (reset)
+	
+	en_delay [3] <= en;
+	en_delay [2] <= en_delay [3];
+	en_delay [1] <= en_delay [2];
+	en_delay [0] <= en_delay [1];
+	
+	if (reset) begin
 		{nior_r, niow_r} <= 2'b11;
+		en_delay [3:0] <= 4'b0;
+	end
 	else
-		if (en)
+		if (en_delay [0])
 			{nior_r, niow_r} <= read ? 2'b01 : 2'b10;
 		else
 			{nior_r, niow_r} <= 2'b11;
