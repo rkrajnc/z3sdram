d93cb486caedb26340a9302c54eec72c76702dcb
Administrator <Administrator@localhost>
2013-07-30 21:02:01 +0400

    up

--- cs8900a.v
+++ cs8900a.v
@@ -1,14 +1,15 @@
 module cs8900a_8bit (
-	clk, reset, stb, addr_i, nDS, addr_o, cs8900_ack
+	clk, reset, stb, ior, iow, addr_i, nDS, addr_o, cs8900_ack
 );
 
 input clk;
 input reset;
 input stb;
+input ior, iow;
 
 input [3:0] nDS;
 input [1:0] addr_i;
-output [3:0] addr_o;
+output reg [3:0] addr_o;
 
 output reg cs8900_ack;
 
@@ -18,29 +19,43 @@ wire [1:0] subaddr;
 // convert nDS [3:0] strobes into low address lines A1, A0
 assign subaddr [1:0] = nDS [3] ? (nDS [2] ? (nDS [1] ? 2'b11 : 2'b10) : 2'b01) : 2'b00;
 
-assign addr_o [3:0] = {addr_i [1:0], subaddr [1:0]};
+//assign addr_o [3:0] = {addr_i [1:0], subaddr [1:0]};
 
-// IORW low to SD valid is max 135ns, need to delay rdy
-parameter TIOR3 = 5'd20; //4'd14;
-reg [4:0] ticks;
+
+//assign addr_o [3:0] = {addr_i [1:0], subaddr [1:0]};
+
+// IORW low to SD valid is max 135ns, need to delay ack
+parameter TIOR3 = 6'd16; //4'd14;
+reg [5:0] ticks;
+
 
 always @(posedge clk) begin
 	if (reset) begin
 		cs8900_ack <= 1'b0;
-		ticks [4:0] <= 5'b0;
+		ticks [5:0] <= 6'b0;
 	end
 	else begin
 	
+	//	addr_o [3:0] <= {addr_i [1:0], subaddr [1:0]};
+	
+	
+	
+		
 		if (stb) begin
-			if (ticks [4:0] != TIOR3) begin
-				ticks [4:0] <= ticks [4:0] + 5'b1;
+//		if (~(ior & iow)) begin
+			if (ticks [4:0] == 5'd0)
+				addr_o [3:0] <= {addr_i [1:0], subaddr [1:0]};
+			if (ticks [5:0] != TIOR3) begin
+				if (~(ior & iow)) begin
+					ticks [5:0] <= ticks [5:0] + 6'b1;
+				end;
 				cs8900_ack <= 1'b0;
 			end
 			else
 				cs8900_ack <= 1'b1;
 		end
 		else begin
-			ticks [4:0] <= 5'b0;
+			ticks [5:0] <= 6'b0;
 			cs8900_ack <= 1'b0;
 		end
 	
--- isa.v
+++ isa.v
@@ -5,7 +5,6 @@ module isa (
 	read,
 	nSLAVEN,
 	
-	terminate,
 	
 	nIOR,
 	nIOW,
@@ -18,7 +17,6 @@ input en;
 input read;
 input nSLAVEN;
 
-input terminate;
 
 output nIOR;
 output nIOW;
@@ -28,25 +26,30 @@ output nIOW;
 reg nior_r;
 reg niow_r;
 
-reg [3:0] en_delay;
+
+reg [5:0] en_delay;
 
 assign nIOR = nior_r | nSLAVEN;
 assign nIOW = niow_r | nSLAVEN;
 
+
+
 always @(posedge clk) begin
 	
-	en_delay [3] <= en;
+	en_delay [5] <= en;
+	en_delay [4] <= en_delay [5];
+	en_delay [3] <= en_delay [4];
 	en_delay [2] <= en_delay [3];
 	en_delay [1] <= en_delay [2];
 	en_delay [0] <= en_delay [1];
 	
 	if (reset) begin
 		{nior_r, niow_r} <= 2'b11;
-		en_delay [3:0] <= 4'b0;
+		en_delay [5:0] <= 6'b0;
 	end
 	else
 		if (en_delay [0])
-			{nior_r, niow_r} <= read ? 2'b01 : 2'b10;
+			{nior_r, niow_r} <= nSLAVEN ? 2'b11 : read ? 2'b01 : 2'b10;
 		else
 			{nior_r, niow_r} <= 2'b11;
 end
